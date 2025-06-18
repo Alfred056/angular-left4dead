74edd414030bddfa75dd9feb41903f8130a1fe05
Alfred056 <alfredovilca374@gmail.com>
2025-06-18 13:34:04 -0500

    pagina de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ReglasComponent } from './pages/reglas/reglas.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'reglas', component: ReglasComponent },
+  { path: 'equipos', component: EquiposComponent },
+  { path: '**', redirectTo: '' } // redirección si no existe la ruta
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ReglasComponent } from './pages/reglas/reglas.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
+import { NavComponent } from './component/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ReglasComponent,
+    EquiposComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
